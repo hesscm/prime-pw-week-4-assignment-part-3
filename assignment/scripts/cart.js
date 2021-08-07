d70e2cb1a30265f6f0f006f86457428f677d3e1b
Chris Hess <chrishessmusic@gmail.com>
2021-08-07 15:21:35 -0500

    completed week 4 assignment part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,6 +1,8 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+//declare two global cariables
 let basket = [];
 const maxItems = 5;
 
@@ -23,7 +25,7 @@ function main() {
   listItems(basket);
 
   //use a loop to add past maxItems
-  for (var i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     addItem(i);
   }
   //list again after loop
@@ -44,6 +46,7 @@ function main() {
   listItems(basket);
 }
 
+//run the main function
 main();
 
 //ADD ITEMS
@@ -60,24 +63,29 @@ function addItem(item) {
 
 //LIST ITEMS
 function listItems(array) {
+  //log text for clarity
   console.log('Listing current items...');
   console.log('Total # of items:', array.length);
-  for (var i = 0; i < array.length; i++) {
+  //loop that lists items in the array on separate lines
+  for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
   }
 }
 
 //EMPTY BASKET
 function empty() {
+  //log text for clarity
   console.log('Basket will now be emptied.');
+  //delete all elements in the basket array
   basket = [];
 }
 
 //CHECK IF BASKET IS FULL
 function isFull() {
+  //return true if basket elements are > or = maxItems
   if (basket.length >= maxItems) {
     return true;
-}
+  }
   else {
     return false;
   }
@@ -85,13 +93,19 @@ function isFull() {
 
 //REMOVE AN ITEM
 function removeItem(item) {
+  //log text for clarity
   console.log(`Preparing to remove "${item}"...`);
   console.log('Searching for item...');
+  //.indexOf returns -1 if the value does not exist
   if (basket.indexOf(item) === -1) {
     console.log('No such item exists in the basket.');
+    //return null if not item is found
     return null;
   }
-  console.log(basket.indexOf(item));
+  //list element # with context
+  console.log(`${item} is element ${basket.indexOf(item)} in the basket.`);
+//remove only the 1 item from the array
   basket.splice(basket.indexOf(item), 1);
+  //log text for clarity
   return console.log(`"${item}" was removed.`);
 }
